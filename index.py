c91281dcac51dee48988dc21ce4765520529c80d
Hafgun <hafpixels06@gmail.com>
2023-05-08 12:25:50 +0200

    added and run some functions i imported

--- index.py
+++ index.py
@@ -1,13 +1,33 @@
+from classes.student import showstudent, printing
+
+import platform
 if __name__ == '__main__':
     # oop
     """une classe
-    commence tjrs par une lettre majuscule et doit etre au sigulier
+    commence tjrs par une lettre majuscule et doit etre au singulier
     """
 
-    class Student:
-        def __init__(self, first_name, last_name):
-            self.first_name = first_name
-            self.last_name = last_name
+    # class Student:
+    #     def __init__(self, first_name, last_name, age):
+    #         self.first_name = first_name
+    #         self.last_name = last_name
+    #         self.age = age
 
-    s = Student("Jean", "Paul")
-    print(s.first_name, s.last_name)
+    #     def __str__(self) -> str:
+    #         return f"{self.first_name}({self.age})"
+
+    # s = Student("Jean", "Paul", 24)
+    # print(s.first_name, s.last_name, s.age)
+    # print(s)
+
+    showstudent()
+
+    printing()
+
+    x = platform.system()
+
+    print(x)
+
+# pip freeze > requirements.txt
+
+# pip install -r requirements.txt

4dab1c242cbd8f489f377903e7d0554f7ec4e21f
Hafgun <hafpixels06@gmail.com>
2023-05-08 10:53:44 +0200

    how to display keys, values or both in dictionary

--- main.py
+++ main.py
@@ -16,27 +16,46 @@ if __name__ == '__main__':
     students = {
         "nom": "IRAKOZE",
         "prenom": "Yves",
-        "adresse": ["kinanira", "kibenga"]
+        "adresse": ["kinanira", "kibenga"],
+        "tel": "75222152"
     }
+    print('--------------------Affichage des cles ---------------\n')
+    for s in students.keys():
+        print(s)
 
-    # print(type(students))
+    print('\n--------------------Affichage des valeurs ---------------\n')
+
+    for s in students.values():
+        print(s)
+
+    print('\n--------------------Affichage des cles et des valeurs ---------------\n')
+
+    for k, v in students.items():
+        print(f"{k} : {v}")
 
     # les sets (n'acceptent pas  la duplicztion contrerement aux listes)
     # fruits = {'Mangue', 'Banane', 'Avocat', 'Mangue'}
+    jours = {"Lundi", "Mardi", "Mercredi",
+             "Jeudi", "Vendredi", "Samedi", "Dimanche"}
+
+    # print('----------Affichage des jours---------------\n')
+
+    # for j in jours:
+    #     print(j)
 
     # Les listes
     fruits = ['Mangue', 'Banane', 'Avocat']
 
-    print('--------------------Affichage avant ajout du nouveau élement---------------\n')
+    # print('--------------------Affichage avant ajout du nouveau élement---------------\n')
 
-    for f in fruits:
-        print(f)
+    # for f in fruits:
+    # print(f)
 
     # fruits.append('Pasteque')
-    fruits.insert(0, "pasteque")
+    # fruits.insert(0, "pasteque")
 
-    print('--------------------Affichage avant apres du nouveau element---------------\n')
+    # print('--------------------Affichage avant apres du nouveau element---------------\n')
 
-    print(fruits)
+    # print(fruits)
 
     print('\n--------------------FIN*-------------------')
